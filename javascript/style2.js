29e279f54354a897d1ebe56e172b5fe16d9461fe
Bubbelicious <62703920+Bubbelicious@users.noreply.github.com>
2020-05-21 15:25:48 +0200

    Update fix

--- javascript/style2.js
+++ javascript/style2.js
@@ -16,9 +16,5 @@ function showSlides(n, no) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-}
   x[slideIndex[no]-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
